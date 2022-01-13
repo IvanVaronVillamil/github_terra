bb306e18ff99087801e5dc1983eb85fc23076842
ivanvaronvillamil <ivaronvillamil@gmail.com>
2022-01-13 15:00:43 -0500

    Test first terra pipeline

--- create_repo.tf
+++ create_repo.tf
@@ -1,4 +1,15 @@
+#Repo for testing
 resource "github_repository" "test-repo" {
+  name        = "test-repo"
+  description = "Test repo from Terraform"
+  visibility  = "private"
+  template {
+    owner      = "IvanVaronVillamil"
+    repository = "java_spring_template"
+  }
+}
+
+resource "github_repository" "test-repo-fr-pipeline" {
   name        = "test-repo"
   description = "Test repo from Terraform"
   visibility  = "private"
