e7458cb01d74f587bf349000daea3464ebd92cfd
IvanVaronVillamil <48997598+IvanVaronVillamil@users.noreply.github.com>
2022-01-14 11:19:57 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "token" {
   type    = string
-  default = "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" # Your Github personal access toke
+  default = "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" # Your Github personal access toke
 }
